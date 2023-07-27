c25ea524cfee8d9f6941f986cddef98faad807c6
xiangqians <shixiangqian.s@gmail.com>
2023-07-27 22:47:16 +0800

    jsch shell定义

--- src/main/java/org/xiangqian/sf/ssh/impl/jsch/JSchShellSshImpl.java
+++ src/main/java/org/xiangqian/sf/ssh/impl/jsch/JSchShellSshImpl.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.time.Duration;
 
 /**
- * {@link ChannelShell}，可执行多行命令
+ * 执行命令（可执行单行、多行命令）
  *
  * @author xiangqian
  * @date 22:37 2022/07/25
@@ -19,11 +19,12 @@ public class JschShellSshImpl extends JschSupport implements Ssh {
 
     protected JschShellSshImpl(String host, int port, String user, String passwd, Duration timeout) throws JSchException {
         super(host, port, user, passwd, timeout);
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public InputStream exec(String cmd, Duration timeout) throws Exception {
-        // ChannelShell执行命令（可以执行多行命令）
+        // ChannelShell执行命令（可执行多行命令）
         ChannelShell channel = (ChannelShell) openChannel(ChannelType.SHELL);
 
         return null;
